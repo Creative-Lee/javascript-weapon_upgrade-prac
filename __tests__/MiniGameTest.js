19ab52dc5a561bcd4dea3ae7765145b4b6b07d41
DOBOB_LAPTOP <dlehgus1110@naver.com>
2022-12-16 21:32:28 +0900

    test: 테스트 가능한 메서드 구조 생성

---
+++ __tests__/MiniGameTest.js
@@ -0,0 +1,10 @@
+const MiniGame = require('../src/MiniGame.js');
+
+const miniGame = new MiniGame();
+miniGame.setNumber(4);
+
+describe('MiniGame 클래스 테스트', () => {
+  describe('play 메서드 테스트', () => {
+    test('', () => {});
+  });
+});
---
+++ __tests__/UpgradeGameTest.js
@@ -0,0 +1,13 @@
+const UpgradeGame = require('../src/UpgradeGame.js');
+
+const upgradeGame = new UpgradeGame();
+
+describe('UpgradeGame 클래스 테스트', () => {
+  describe('getProbabilty 메서드 테스트', () => {
+    test('', () => {});
+  });
+
+  describe('upgradeWeapon 메서드 테스트', () => {
+    test('', () => {});
+  });
+});
